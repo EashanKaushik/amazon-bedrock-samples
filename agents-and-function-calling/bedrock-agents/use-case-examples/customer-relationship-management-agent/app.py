61cba8e9bc88ba8ddafa2d5a221f0244f245b384
Eashan Kaushik <50113394+EashanKaushik@users.noreply.github.com>
2025-02-23 15:10:01 -0500

    Update app.py

--- agents-and-function-calling/bedrock-agents/use-case-examples/customer-relationship-management-agent/app.py
+++ agents-and-function-calling/bedrock-agents/use-case-examples/customer-relationship-management-agent/app.py
@@ -5,25 +5,39 @@ import pandas as pd
 import numpy as np
 import util
 from argparse import ArgumentParser
-import clipboard
+
 
 parser = ArgumentParser()
 parser.add_argument("--environmentName", type=str, default=None)
 
 args = parser.parse_args()
 
-def on_copy_click(text):
-    st.session_state.copied.append(text)
-    clipboard.copy(text)
-
 environmentName = args.environmentName
 bedrock = util.BedrockAgent(environmentName)
+
 
 st.set_page_config(layout="wide", page_title="CRM")
 
 st.title("Customer Relation Manager (CRM)")
 heading_column1, heading_column_space, heading_column2 = st.columns((6, 2, 2))
 
+def create_copy_button(text_to_copy):
+    button_id = "copyButton" + text_to_copy
+    
+    button_html = f"""<button id='{button_id}'>Copy</button>
+    <script>
+    document.getElementById('{button_id}').onclick = function() {{
+        navigator.clipboard.writeText('{text_to_copy}').then(function() {{
+            console.log('Async: Copying to clipboard was successful!');
+        }}, function(err) {{
+            console.error('Async: Could not copy text: ', err);
+        }});
+    }}
+    </script>"""
+    
+    st.markdown(button_html, unsafe_allow_html=True)
+
+    
 with heading_column1:
     st.subheader(":grey[Amazon Bedrock Agents]")
 
@@ -80,14 +94,14 @@ for index, chat in enumerate(st.session_state["chat_history"]):
         else:
             st.markdown(chat["prompt"])
 
-if prompt := st.chat_input("Ask the bot about customer..."):
-    st.session_state["chat_history"].append({"role": "human", "prompt": prompt})
+if text_to_copy := st.chat_input("Ask the bot about customer...", key="text_to_copy"):
+    st.session_state["chat_history"].append({"role": "human", "prompt": text_to_copy})
 
     with st.chat_message("human"):
-        st.markdown(prompt)
+        st.markdown(text_to_copy)
 
     with st.chat_message("assistant"):
-        response_text, trace_text = bedrock.invoke_agent(prompt)
+        response_text, trace_text = bedrock.invoke_agent(text_to_copy)
         st.session_state["chat_history"].append(
             {"role": "assistant", "prompt": response_text, "trace": trace_text}
         )
@@ -96,9 +110,10 @@ if prompt := st.chat_input("Ask the bot about customer..."):
         if len(st.session_state.copied) > 5:
             st.session_state.copied.pop(0)
             
-        st.button("Copy 📋", on_click=on_copy_click, args=(response_text,))
+        create_copy_button(st.session_state.text_to_copy)
         # if col3.checkbox('Trace', key=len(st.session_state["chat_history"]), label_visibility="hidden"):
         # col2.markdown(trace_text)
-    
+
+
 for text in st.session_state.copied:
     st.toast(f"Copied to clipboard: {text}", icon='✅' )
